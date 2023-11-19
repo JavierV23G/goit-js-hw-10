9f0ab43455323b089713fe20df57ea10eebaf7fe
JavierV23G <127901386+JavierV23G@users.noreply.github.com>
2023-11-19 16:06:45 -0500

    Still error,ordered div changed

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,32 +1,47 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_SIFTvSVX5Hqh6v9SUd9xSKCQz6ArdrcxidgwATNB79K7LkHNMPbHMK5TISKIpGg2";
+import axios from 'axios';
 
 export function fetchCatByBreed(selectedBreedId) {
-    const catInfo = document.querySelector('.cat-info'); 
-    
-    axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${selectedBreedId}`)
-        .then(response => {
-            const catData = response.data;
-            if (catData && catData.length > 0 && catData[0].breeds && catData[0].breeds.length > 0) {
-                const catImage = catData[0].url;
-                const catBreed = catData[0].breeds[0];
+    const catInfo = document.querySelector('.cat-info');
 
-                catInfo.innerHTML = `
-                    <img src="${catImage}" alt="Cat Image">
-                    <h2>${catBreed.name}</h2>
-                    <p><strong>Description:</strong> ${catBreed.description}</p>
-                    <p><strong>Temperament:</strong> ${catBreed.temperament}</p>
-                `;
-                
-                const imageElement = catInfo.querySelector('img');
-                imageElement.classList.add('catPicture');
-                
-            } else {
-                catInfo.innerHTML = '<p>No cat data available</p>';
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching cat data:', error);
-            catInfo.innerHTML = '<p>Oops! Something went wrong while fetching cat data.</p>';
-        });
+    axios
+    .get(
+        `https://api.thecatapi.com/v1/images/search?breed_ids=${selectedBreedId}`
+    )
+    .then(response => {
+        const catData = response.data;
+        if (
+        catData &&
+        catData.length > 0 &&
+        catData[0].breeds &&
+        catData[0].breeds.length > 0
+        ) {
+        const catImage = catData[0].url;
+        const catBreed = catData[0].breeds[0];
+
+        const imgElement = document.createElement('img');
+        imgElement.src = catImage;
+        imgElement.alt = 'Cat Image';
+        imgElement.classList.add('catPicture');
+
+      // Crear el contenido del ul
+        const ulElement = document.querySelector('.cat-completeInfo');
+        ulElement.innerHTML = `
+        <li><h2>${catBreed.name}</h2></li>
+        <li><p><strong>Description:</strong> ${catBreed.description}</p></li>
+        <li><p><strong>Temperament:</strong> ${catBreed.temperament}</p></li>
+        `;
+        
+        catInfo.innerHTML = '';
+
+        catInfo.appendChild(imgElement);
+        catInfo.appendChild(ulElement);
+        } else {
+        catInfo.innerHTML = '<p>No cat data available</p>';
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching cat data:', error);
+        catInfo.innerHTML =
+        '<p>Oops! Something went wrong while fetching cat data.</p>';
+    });
 }
